be731cf86de0da0f681ea9413e93f4f71c3db762
jsjackson263 <jsjackson263@users.noreply.github.com>
2018-01-18 13:07:40 +1300

    Initial Commit

---
+++ src/main/java/info/jsjackson/githubtest/GithubtestApplication.java
@@ -0,0 +1,12 @@
+package info.jsjackson.githubtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubtestApplication.class, args);
+	}
+}
